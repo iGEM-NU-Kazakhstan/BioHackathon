2b1b937a1f782458c7b44a2ebcb5d0af9c94595f
ychfxdx <92357151+ychfxdx@users.noreply.github.com>
2021-10-12 09:34:38 +0300

    Add files via upload

---
+++ Code Submissions/Team 12/mission_failed.py
@@ -0,0 +1,16 @@
+
+
+"""
+
+Oh no! There's nothing here! 
+
+
+We are so sorry, but it is officially declared that
+team 12 with three biology warriors was defeated by 
+almighty Maths and Programming lack of skills.
+
+
+Press F
+
+"""
+
