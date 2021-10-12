3e1dc789229cdd0c079b0abba8ce20825dd9c6af
CapYangWenli <61187982+CapYangWenli@users.noreply.github.com>
2021-10-12 11:54:41 +0600

    Update main.py

--- Code Submissions/Chrispies/main.py
+++ Code Submissions/Chrispies/main.py
@@ -68,8 +68,6 @@ if usr2.lower() == 'y':
         print(seq)
     
     
-usr4 = input("\nDo you want to choose how to change restrictive sequences? (y/n):")
-
 lst = []
 
 # Finding and indexing restrictive sequence on DNA
