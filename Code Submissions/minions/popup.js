ca719fa4c5386a75c7f3ae34de960035e3f3350d
Nazekaaaa <92355906+Nazekaaaa@users.noreply.github.com>
2021-10-12 12:25:58 +0600

    Add files via upload

---
+++ Code Submissions/minions/popup.js
@@ -0,0 +1,40 @@
+document.body.addEventListener('click', function(e) {
+    let target = e.target;
+    if (target.nodeName === 'BUTTON') {
+        if (target.name === 'reset') {
+            chrome.storage.sync.clear(function(data){
+                chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+                    chrome.tabs.reload(tab[0].id);
+                });
+            });
+        }
+        changeBG(target.name);
+    }
+    e.stopPropagation();
+});
+function changeBG(color) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'document.body.style.backgroundColor = "#' + color + '";'}
+        );
+    });
+    var el = document.getElementsByTagName("*");
+    for (var i=0; i < el.length; i++) 
+    {    
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {code: 'document.body.style.color = "#' + color + '";'}
+            );
+        });
+        
+    }
+     storeColor(color);
+               
+}
+
+
+function storeColor(bgColor) {
+    chrome.storage.sync.set({currentColor: bgColor});
+}
